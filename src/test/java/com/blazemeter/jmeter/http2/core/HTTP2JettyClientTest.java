eaeaaa2bf24092f6875ecdcb84af562f0b3954b1
Tony Morantes <tony@capicua.com.uy>
2021-09-29 16:13:15 -0400

    Renaming variable

--- src/test/java/com/blazemeter/jmeter/http2/core/HTTP2JettyClientTest.java
+++ src/test/java/com/blazemeter/jmeter/http2/core/HTTP2JettyClientTest.java
@@ -67,7 +67,7 @@ public class HTTP2JettyClientTest {
   private static final int SERVER_PORT = 6666;
   private static final String BASIC_HTML_TEMPLATE = "<!DOCTYPE html><html><head><title>Page "
       + "Title</title></head><body><div><img src=%s></div></body></html>";
-  private static final byte[] content = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  private static final byte[] BINARY_RESPONSE_BODY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
   @Rule
   public final JUnitSoftAssertions softly = new JUnitSoftAssertions();
@@ -124,7 +124,8 @@ public class HTTP2JettyClientTest {
     startServer(createGetServerResponse());
     HTTPSampleResult result = client.sample(sampler, new URL(HTTPConstants.PROTOCOL_HTTPS,
         HOST_NAME, SERVER_PORT, SERVER_PATH_200_GZIP), HTTPConstants.GET, false, 0);
-    softly.assertThat(HTTP2JettyClientTest.content).isEqualTo(result.getResponseData());
+    softly.assertThat(HTTP2JettyClientTest.BINARY_RESPONSE_BODY)
+        .isEqualTo(result.getResponseData());
   }
 
   private HttpServlet createGetServerResponse() {
@@ -160,13 +161,13 @@ public class HTTP2JettyClientTest {
             return;
           case SERVER_PATH_200_FILE_SENT:
             resp.setContentType("image/png");
-            byte [] requestBody = req.getInputStream().readAllBytes();
+            byte[] requestBody = req.getInputStream().readAllBytes();
             resp.getOutputStream().write(requestBody);
             return;
           case SERVER_PATH_200_GZIP:
             resp.addHeader("Content-Encoding", "gzip");
             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(resp.getOutputStream());
-            gzipOutputStream.write(HTTP2JettyClientTest.content);
+            gzipOutputStream.write(HTTP2JettyClientTest.BINARY_RESPONSE_BODY);
             gzipOutputStream.close();
             break;
         }
